461f3bc058ebd841ef01a0f88f627b88761a2d59
shizhijie <18232529363@163.com>
2019-03-04 17:31:01 +0800

    webservice

--- webservice_04_jaxws_first_client/src/cn/itcast/webservice/jaxws/server/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://server.jaxws.webservice.itcast.cn/")
-package cn.itcast.webservice.jaxws.server;
--- webservice_07_cxf_client/src/cn/itcast/webservice/cxf/server/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://server.cxf.webservice.itcast.cn/")
-package cn.itcast.webservice.cxf.server;
